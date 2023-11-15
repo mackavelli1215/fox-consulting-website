447abf39b0e76b276c31301e4141c42b89cb0284
mackavelli1215 <82186773+mackavelli1215@users.noreply.github.com>
2023-11-15 23:03:35 +0000

    created templates, added Bootstrap, routed pages

--- fox_webapp.py
+++ fox_webapp.py
@@ -1,12 +1,25 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # Create Flask app
 app = Flask(__name__)
 
+
 # Define routes - registers routes to app
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def home():
+  return render_template('home.html')
 
+
+@app.route('/client')
+def client():
+  return render_template('client.html')
+
+
+@app.route('/staff')
+def staff():
+  return render_template('staff.html')
+
+
+# Start the app
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', debug=True)
+  app.run(host='0.0.0.0', debug=True)
